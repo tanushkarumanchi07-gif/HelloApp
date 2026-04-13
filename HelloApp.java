1b0784ed32b0f9762739bdb0d1ee6a76a00d5329
tanushkarumanchi07-gif <tanushkarumanchi07@gmail.com>
2026-04-13 15:10:22 +0530

    Improve greeting logic in HelloApp
    Refactor greeting logic to handle no arguments case and concatenate multiple names.

--- HelloApp.java
+++ HelloApp.java
@@ -1,13 +1,18 @@
 public class HelloApp {
     public static void main(String[] args) {
-        String name;
-
-        if (args.length > 0) {
-            name = args[0];
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
         } else {
-            name = "World";
+            StringBuilder nameBuilder = new StringBuilder();
+            
+            for (String name : args) {
+                nameBuilder.append(name).append(", ");
+            }
+            
+            if (nameBuilder.length() > 0) {
+                String names = nameBuilder.substring(0, nameBuilder.length() - 2);
+                System.out.println("Hello, " + names + "!");
+            }
         }
-
-        System.out.println("Hello, " + name + "!");
     }
 }
