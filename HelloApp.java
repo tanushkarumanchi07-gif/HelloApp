1eed8e72f903407eef4cbbeaf614a99f6f0a882b
tanushkarumanchi07-gif <tanushkarumanchi07@gmail.com>
2026-04-13 15:04:13 +0530

    Update HelloApp.java

--- HelloApp.java
+++ HelloApp.java
@@ -1,13 +1,5 @@
 public class HelloApp {
     public static void main(String[] args) {
-        String name;
-
-        if (args.length > 0) {
-            name = args[0];
-        } else {
-            name = "World";
-        }
-
-        System.out.println("Hello, " + name + "!");
+        System.out.println("Hello World");
     }
 }
